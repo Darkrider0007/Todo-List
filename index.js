f92d5a9b262ce16dcc058d0682ea8d232fafe536
Rohan Gope <rg2512@it.jgec.ac.in>
2023-03-20 17:59:20 +0530

    Update PORT

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.post("/delete",(req,res)=>{
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT ||3000, function() {
   console.log("Server running on port 3000.");
 });
 
